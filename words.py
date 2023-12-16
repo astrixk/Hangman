ee3b5e27fd25a3ffddd5bd290cc6a94fd21e5801
Sampath Guruprasad <59566243+astrixk@users.noreply.github.com>
2023-12-16 21:13:37 +0530

    Add files via upload

---
+++ words.py
@@ -0,0 +1,16 @@
+word_list = [
+    "delirious", "teaching", "start", "addition", "scared", "public", "woozy", 
+    "seashore", "flash", "disappear", "fail", "tray", "grandmother", "receive", 
+    "guiltless", "savory", "time", "interesting", "tip", "rings", "jelly", 
+    "scientific", "bad", "fire", "common", "rifle", "throat", "agreeable", 
+    "vest", "curve", "thick", "escape", "ritzy", "improve", "ashamed", 
+    "determined", "pickle", "romantic", "arm", "annoyed", "decay", "noxious", 
+    "live", "beg", "meeting", "close", "likeable", "changeable", "military", 
+    "lamentable", "weight", "sin", "natural", "sneeze", "dry", "jog", "brainy", 
+    "terrific", "stop", "opposite", "rob", "fasten", "snow", "unsightly", "own", 
+    "grieving", "melt", "forgetful", "bed", "store", "obese", "fast", 
+    "ill-informed", "funny", "sudden", "maddening", "next", "lethal", "mouth", 
+    "poke", "interest", "cushion", "evasive", "towering", "rightful", "idea", 
+    "knowledgeable", "birds", "halting", "cough", "wonderful", "mushy", "shelf", 
+    "tacit", "striped", "chop", "staking", "humor", "well-groomed", "wanting"
+]
